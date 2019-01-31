5065c4d0ab1c12f7f353c2fdfd2434e961c339ad
brooklyn <enclaveit@123>
2019-01-31 14:44:37 +0700

    Constants: Edit ISO and ANDROID HASHED APP ID

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
     //Browserstack setup
     public static final String BS_USERNAME = "perriduncan1";
     public static final String BS_ACCESSKEY = "FXQUqgscz3gX5NzxH61k";
-    public static final String IOS_HASHED_APP_ID = "5be9e639ade4aa41618d68ccb425f0516b0a1407";
-    public static final String ANDROID_HASHED_APP_ID = "b54a2170df2d3fdc9c6f388fac28e63e2c5cc1be";
+    public static final String IOS_HASHED_APP_ID = "050d52cac01da8a38bf3230d82ea82640afcb3dd";
+    public static final String ANDROID_HASHED_APP_ID = "04af28d4eca3206b54b700c71b1b0e8899d98ce2";
 
     public static final int TIME_OUT = 5;
 
