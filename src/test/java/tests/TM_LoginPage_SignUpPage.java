cd75bffb2ef887d4639ccefbf84e6f6d4def9c05
Sarah <sarah@enclave.vn>
2019-03-13 09:38:09 +0700

    SignUpPage: Add test case descriptions

--- src/test/java/tests/TM_LoginPage_SignUpPage.java
+++ src/test/java/tests/TM_LoginPage_SignUpPage.java
@@ -1,21 +1,28 @@
 package tests;
 
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 
 public class TM_LoginPage_SignUpPage extends BaseTestClass {
 
 
     @Test(priority = 1)
+    @Description("Load Sign Up screen from Settings")
     public void SignUpPageTCPCOM_001() {
        signUpPage.TestCasePCOM_001();
     }
 
     @Test(priority = 2)
+    @Description("Close Sign Up screen from Settings")
     public void SignUpPageTCPCOM_003() {
         signUpPage.TestCasePCOM_003();
     }
 
     @Test(priority = 3)
-    public void SignUpPageTCPCOM_005() { signUpPage.TestCasePCOM_005(); }
+    @Description("Put phone to sleep on Sign Up screen and wake phone up to home screen")
+    public void SignUpPageTCPCOM_004() { signUpPage.TestCasePCOM_004(); }
 
+    @Test(priority = 4)
+    @Description("Put phone to sleep on Sign Up screen and wake phone up to home screen")
+    public void SignUpPageTCPCOM_005() { signUpPage.TestCasePCOM_005(); }
 }
