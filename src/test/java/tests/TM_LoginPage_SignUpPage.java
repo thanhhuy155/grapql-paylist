dc2b9b6505d684d52dad1e42f49cf2caddcd1210
Sarah <sarah@enclave.vn>
2019-03-18 13:10:16 +0700

    Comment @Priority in SignUpPage Test

--- src/test/java/tests/TM_LoginPage_SignUpPage.java
+++ src/test/java/tests/TM_LoginPage_SignUpPage.java
@@ -1,28 +1,27 @@
 package tests;
 
 import io.qameta.allure.Description;
-import org.testng.annotations.Test;
 
 public class TM_LoginPage_SignUpPage extends BaseTestClass {
 
 
-    @Test(priority = 1)
+//    @Test(priority = 1)
     @Description("Load Sign Up screen from Settings")
     public void SignUpPageTCPCOM_001() {
        signUpPage.TestCasePCOM_001();
     }
 
-    @Test(priority = 2)
+//    @Test(priority = 2)
     @Description("Close Sign Up screen from Settings")
     public void SignUpPageTCPCOM_003() {
         signUpPage.TestCasePCOM_003();
     }
 
-    @Test(priority = 3)
+//    @Test(priority = 3)
     @Description("Put phone to sleep on Sign Up screen and wake phone up to home screen")
     public void SignUpPageTCPCOM_004() { signUpPage.TestCasePCOM_004(); }
 
-    @Test(priority = 4)
+//    @Test(priority = 4)
     @Description("Put phone to sleep on Sign Up screen and wake phone up to home screen")
     public void SignUpPageTCPCOM_005() { signUpPage.TestCasePCOM_005(); }
 }
