b0553492513fc35cfa8dc3d51af46083689d702e
ENCLAVEIT <enclaveit@123>
2019-03-20 15:35:30 +0700

    Constant: Edit iOS Hasded Id and version for SprintH

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     //Browserstack setup
     public static final String BS_USERNAME = "perriduncan1";
     public static final String BS_ACCESSKEY = "FXQUqgscz3gX5NzxH61k";
-    public static final String IOS_HASHED_APP_ID = "050d52cac01da8a38bf3230d82ea82640afcb3dd";
+    public static final String IOS_HASHED_APP_ID = "c1ce4c58e206d49d52f7e312987e2de57f8e7299";
     public static final String ANDROID_HASHED_APP_ID = "e4948bacd16caee0721d2dcb9d4f3aeaa5d1dcbf";
 
     public static final int TIME_OUT = 5;
@@ -42,7 +42,7 @@ public class Constants {
 
     public static class APP_VERSION {
         public static final String ANDROID = "4.1.0 (64)";
-        public static final String IOS = "4.1.0 (143)";
+        public static final String IOS = "4.1.0 (144)";
     }
 
     public static class LABLE {
