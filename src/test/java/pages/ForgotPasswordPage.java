a2c1b8617db1a3e3a7c28ca6ee0224c1c8352094
Sarah <sarah@enclave.vn>
2019-03-20 13:21:09 +0700

    ForgotPassword: Add steps descriptions

--- src/test/java/pages/ForgotPasswordPage.java
+++ src/test/java/pages/ForgotPasswordPage.java
@@ -12,6 +12,7 @@ import io.appium.java_client.android.nativekey.KeyEvent;
 import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.Point;
 import org.testng.Assert;
+import scenarios.AppiumController;
 import utils.Constants;
 import utils.Utils;
 
@@ -31,26 +32,23 @@ public class ForgotPasswordPage extends BasePage {
         settingElements= new SettingElements(driver);
         feedListElements= new FeedListElements(driver);
         loginElements = new LoginElements(driver);
-
     }
 
 
     public void TestCasePCOM_001() {
+        //1. Open the Philly.com app to "Log In" screen
         lauchApp();
         waitForVisibilityOf(commonElements.bottomTab);
         commonElements.settingTab.click();
         waitForVisibilityOf(settingElements.logInOrSignUp);
 
-        //Click Log In or Sign Up
         settingElements.logInOrSignUp.click();
         Utils.waitForElementVisible(appiumDriver, loginElements.actionBarTitle);
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
 
-        //Click Forgot Password link
+        //2. Click "Forgot password?" link
         loginElements.forgotPasswordLink.click();
-        //Check Forgot Password screen
         Assert.assertTrue(Utils.checkElementExist(loginElements.forgotPasswordMessage),"Check forgot message displays");
         loginElements.assertValue(loginElements.forgotPasswordMessage, Constants.LOGIN.FORGOT_PASSWORD_MESSAGE);
         Assert.assertTrue(Utils.checkElementExist(loginElements.email),"Check Email field displays ");
@@ -58,76 +56,88 @@ public class ForgotPasswordPage extends BasePage {
 
         //Extra step to hidden keyboard
         loginElements.hiddenKeyboard(appiumDriver,loginElements.email);
-
         loginElements.assertValue(loginElements.forgotEmailMessage, Constants.LOGIN.FORGOT_EMAIL_ADDRESS_MESSAGE);
     }
 
 
     public void TestCasePCOM_002() {
+        //1. Open the Philly.com app to Log In screen from Settings tab.
         lauchApp();
         waitForVisibilityOf(commonElements.bottomTab);
         commonElements.settingTab.click();
         waitForVisibilityOf(settingElements.logInOrSignUp);
 
-        //Click Log In or Sign Up
         settingElements.logInOrSignUp.click();
         Utils.waitForElementVisible(appiumDriver, loginElements.actionBarTitle);
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
 
-        //Click Forgot Password link
+        //2. Click on "Forgot password?" link
         loginElements.forgotPasswordLink.click();
         Assert.assertTrue(Utils.checkElementExist(loginElements.forgotPasswordMessage),"Check forgot message displays");
 
+
+        //3. Put the device to sleep with Philly.com app on the screen.
         lockDevice();
+
+
+        //4. Wake the device and unlock (if applicable)
+        //5. Observe the Philly.com app opened to "Reset Password" screen.
         unLockDevice();
         Utils.sleep(Constants.SHORTTIME);
 
-        //Click Back button to return Log In screen
-        loginElements.closeActionButton.click();
-        //Check Login screen
-        loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
-        //Click close Log In screen
+        //6. Return to Log In screen and cancel Log In.
         loginElements.closeActionButton.click();
-        //Check Setting screen displays
+        loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
+        loginElements.closeActionButton.click();
         waitForVisibilityOf(settingElements.logInOrSignUp);
         loginElements.assertValue(settingElements.logInOrSignUp, Constants.LOGIN.LOG_IN_OR_SIGN_UP);
 
-        //Click all tabs
-        commonElements.homeTab.click();
-        commonElements.exploreTab.click();
-        commonElements.settingTab.click();
-        commonElements.bookMarkTab.click();
 
+        //7. Click all tabs in navigation
+        //8. Observe the tabs load without crashing.
+        commonElements.homeTab.click();
+        Assert.assertTrue(commonElements.homeTab.isSelected());
+        commonElements.exploreTab.click();
+        Assert.assertTrue(commonElements.exploreTab.isSelected());
+        commonElements.settingTab.click();
+        Assert.assertTrue(commonElements.settingTab.isSelected());
+        commonElements.bookMarkTab.click();
+        Assert.assertTrue(commonElements.bookMarkTab.isSelected());
+
+        //9. Terminate the app by double clicking the home button and swiping up.
         ((AndroidDriver) appiumDriver).pressKey(new KeyEvent(AndroidKey.HOME));
+        Assert.assertFalse(commonElements.homeTab.isDisplayed());
     }
 
     public void TestCasePCOM_006() {
+
+        //1. Open the Philly.com app to Settings tab.
         lauchApp();
         waitForVisibilityOf(commonElements.bottomTab);
         commonElements.settingTab.click();
         Utils.sleep(Constants.SHORTTIME);
         waitForVisibilityOf(settingElements.logInOrSignUp);
 
-        //Click Log In or Sign Up
+
+        //2. Click "Log In or Sign Up" option
         settingElements.logInOrSignUp.click();
         Utils.waitForElementVisible(appiumDriver, loginElements.actionBarTitle);
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
 
-        //Click Forgot Password link
+        //3. Click "Forgot Password?" link
+        //4. Observe "Reset Password" screen display
         loginElements.forgotPasswordLink.click();
-
-        //Check Reset Password screen displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.forgotPasswordMessage),"Check forgot message displays");
 
-        //Click Home to terminate
+
+        //5. Terminate the app by double clicking the home button and swiping up
         ((AndroidDriver) appiumDriver).pressKey(new KeyEvent(AndroidKey.HOME));
 
-        //Open app
+
+        //6. Open the Philly.com app from home screen
         lauchApp();
         waitForVisibilityOf(feedListElements.feedItemTitleTopStory);
         Assert.assertTrue(Utils.checkElementExist(feedListElements.feedItemTitleTopStory));
@@ -135,177 +145,170 @@ public class ForgotPasswordPage extends BasePage {
 
 
     public void TestCasePCOM_007() {
+        //1. Open the Philly.com app to "Log In" screen
         lauchApp();
         waitForVisibilityOf(commonElements.bottomTab);
         commonElements.settingTab.click();
         waitForVisibilityOf(settingElements.logInOrSignUp);
         loginElements.assertValue(settingElements.logInOrSignUp, Constants.LOGIN.LOG_IN_OR_SIGN_UP);
 
-        //Click Log In or Sign Up
         settingElements.logInOrSignUp.click();
         Utils.waitForElementVisible(appiumDriver, loginElements.actionBarTitle);
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
 
-        //Click Forgot Password link
+        //2. Click "Forgot Password?" link
         loginElements.forgotPasswordLink.click();
-        //Check Reset Password screen displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.forgotPasswordMessage),"Check forgot message displays");
 
-        //Leave Email as blank
+        //3. Leave Email field as blank
 
-        //Click Send Email
+
+        //4. Click on "Send Email" button
         loginElements.sendEmailButton.click();
-        //Check Error message displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.messageEmail));
         loginElements.assertValue(loginElements.messageEmail, Constants.LOGIN.BLANK_EMAIL_ERROR_MESSAGE);
     }
 
 
     public void TestCasePCOM_008() {
+        //1. Open the Philly.com app to "Log In" screen
         lauchApp();
         waitForVisibilityOf(commonElements.bottomTab);
         commonElements.settingTab.click();
         waitForVisibilityOf(settingElements.logInOrSignUp);
         loginElements.assertValue(settingElements.logInOrSignUp, Constants.LOGIN.LOG_IN_OR_SIGN_UP);
 
-        //Click Log In or Sign Up
         settingElements.logInOrSignUp.click();
         Utils.waitForElementVisible(appiumDriver, loginElements.actionBarTitle);
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
 
-        //Click Forgot Password link
+        //2. Click "Forgot Password?" link
         loginElements.forgotPasswordLink.click();
         //Check Reset Password screen displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.forgotPasswordMessage),"Check forgot message displays");
 
-        //Leave Email as blank
 
-        //Click outside Email file
+        //3. Leave Email field as blank
+
+
+        //4. Click outside
         loginElements.hiddenKeyboard(appiumDriver, loginElements.sendEmailButton);
-        //Check Error message displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.messageEmail));
         loginElements.assertValue(loginElements.messageEmail, Constants.LOGIN.BLANK_EMAIL_ERROR_MESSAGE);
     }
 
 
     public void TestCasePCOM_009() {
+        //1. Open the Philly.com app to "Log In" screen
         lauchApp();
         waitForVisibilityOf(commonElements.bottomTab);
         commonElements.settingTab.click();
         waitForVisibilityOf(settingElements.logInOrSignUp);
         loginElements.assertValue(settingElements.logInOrSignUp, Constants.LOGIN.LOG_IN_OR_SIGN_UP);
 
-        //Click Log In or Sign Up
         settingElements.logInOrSignUp.click();
         Utils.waitForElementVisible(appiumDriver, loginElements.actionBarTitle);
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
 
-        //Click Forgot Password link
+        //2. Click "Forgot Password?" link
         loginElements.forgotPasswordLink.click();
-        //Check Reset Password screen displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.forgotPasswordMessage),"Check forgot message displays");
 
-        //Enter invalid Email
+
+        //3. Enter invalid email format to Email field
         loginElements.setValue(loginElements.email, "fairlyenclave.vn");
 
-        //Click Send Email
+
+        //4. Click on "Send Email" button
         loginElements.sendEmailButton.click();
-        //Check Error message displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.messageEmail));
         loginElements.assertValue(loginElements.messageEmail, Constants.LOGIN.INVALID_EMAIL_ERROR_MESSAGE);
     }
 
+
     public void TestCasePCOM_010() {
+        //1. Open the Philly.com app to "Log In" screen
         lauchApp();
         waitForVisibilityOf(commonElements.bottomTab);
         commonElements.settingTab.click();
         waitForVisibilityOf(settingElements.logInOrSignUp);
         loginElements.assertValue(settingElements.logInOrSignUp, Constants.LOGIN.LOG_IN_OR_SIGN_UP);
 
-        //Click Log In or Sign Up
         settingElements.logInOrSignUp.click();
         Utils.waitForElementVisible(appiumDriver, loginElements.actionBarTitle);
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
 
-        //Click Forgot Password link
+        //2. Click "Forgot Password?" link
         loginElements.forgotPasswordLink.click();
-        //Check Reset Password screen displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.forgotPasswordMessage),"Check forgot message displays");
 
-        //Enter invalid Email format
+
+        //3. Enter invalid email format to Email field
         loginElements.setValue(loginElements.email, "fairlyenclave.vn");
 
-        //Click outside Email
-        loginElements.hiddenKeyboard(appiumDriver,loginElements.email);
 
-        //Check Error message displays
+        //4. Click outside
+        loginElements.hiddenKeyboard(appiumDriver,loginElements.email);
         Assert.assertTrue(Utils.checkElementExist(loginElements.messageEmail));
         loginElements.assertValue(loginElements.messageEmail, Constants.LOGIN.INVALID_EMAIL_ERROR_MESSAGE);
     }
 
 
     public void TestCasePCOM_011() {
+        //1. Open the Philly.com app to "Log In" screen
         lauchApp();
         waitForVisibilityOf(commonElements.bottomTab);
         commonElements.settingTab.click();
         waitForVisibilityOf(settingElements.logInOrSignUp);
         loginElements.assertValue(settingElements.logInOrSignUp, Constants.LOGIN.LOG_IN_OR_SIGN_UP);
 
-        //Click Log In or Sign Up
         settingElements.logInOrSignUp.click();
         Utils.waitForElementVisible(appiumDriver, loginElements.actionBarTitle);
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
 
-        //Click Forgot Password link
+        //2. Click "Forgot Password?" link
         loginElements.forgotPasswordLink.click();
-        //Check Reset Password screen displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.forgotPasswordMessage),"Check forgot message displays");
 
-        //Enter valid Email format
+
+        //3. Enter valid email to Email field
         loginElements.setValue(loginElements.email, Constants.LOGIN_EMAIL);
 
-        //Click Send Email Email
+
+        //4. Click on "Send Email" button
         loginElements.sendEmailButton.click();
         Utils.sleep(Constants.SHORTTIME);
-        //Check Success screen display
         Assert.assertTrue(Utils.checkElementExist(loginElements.successImage));
         loginElements.assertValue(loginElements.successMessage, Constants.LOGIN.SEND_EMAIL_SUCCESS_MESSAGE);
     }
 
 
     public void TestCasePCOM_013() {
+        //1. Open the Philly.com app to "Log In" screen
         lauchApp();
         waitForVisibilityOf(commonElements.bottomTab);
         commonElements.settingTab.click();
         waitForVisibilityOf(settingElements.logInOrSignUp);
         loginElements.assertValue(settingElements.logInOrSignUp, Constants.LOGIN.LOG_IN_OR_SIGN_UP);
-
-        //Click Log In or Sign Up
         settingElements.logInOrSignUp.click();
         Utils.waitForElementVisible(appiumDriver, loginElements.actionBarTitle);
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
 
-        //Click Forgot Password link
+        //2. Click "Forgot password?" link
         loginElements.forgotPasswordLink.click();
-        //Check Reset Password screen displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.forgotPasswordMessage),"Check forgot message displays");
 
-        //Click hidden keyboard
+        //3. Hide the keyboard
         loginElements.hiddenKeyboard(appiumDriver, loginElements.email);
 
-        //Click the phone number on the message on Reset Password
+
+        //4. Click the phone number on the message at bottom
         TouchAction touchAction = new TouchAction(appiumDriver);
         Point point = loginElements.forgotEmailMessage.getLocation();
 
@@ -314,53 +317,55 @@ public class ForgotPasswordPage extends BasePage {
 
         touchAction.tap(PointOption.point(pointX,pointY)).perform();
 
+         if(!AppiumController.executionOS.equals(AppiumController.OS.ANDROID_BROWSERSTACK)) {
+            //Check Dialer screen displays
+            waitForVisibilityOf(loginElements.dialerWindow);
+            Assert.assertTrue(Utils.checkElementExist(loginElements.dialerWindow), "Check dialer window display: ");
+            //Check dialer with the Number filled in
+            loginElements.assertValue(loginElements.customerServiceNumber, Constants.LOGIN.CUSTOMER_SERVICE_NUMBER);
 
-        //Check Dialer screen displays
-        waitForVisibilityOf(loginElements.dialerWindow);
-        Assert.assertTrue(Utils.checkElementExist(loginElements.dialerWindow),"Check dialer window display: ");
-        //Check dialer with the Number filled in
-        loginElements.assertValue(loginElements.customerServiceNumber, Constants.LOGIN.CUSTOMER_SERVICE_NUMBER);
 
-        //Return app by click back button
-        ((AndroidDriver) appiumDriver).pressKey(new KeyEvent(AndroidKey.BACK));
-        Utils.sleep(Constants.SHORTTIME);
-        //Check Reset Password screen is being returned
-        Assert.assertTrue(Utils.checkElementExist(loginElements.sendEmailButton));
+            //5. Return to the app
+            ((AndroidDriver) appiumDriver).pressKey(new KeyEvent(AndroidKey.BACK));
+            Utils.sleep(Constants.SHORTTIME);
+            //Check Reset Password screen is being returned
+            Assert.assertTrue(Utils.checkElementExist(loginElements.sendEmailButton));
+        }
     }
 
 
     public void TestCasePCOM_015() {
+        //1. Open the Philly.com app to "Log In" screen
         lauchApp();
         waitForVisibilityOf(commonElements.bottomTab);
         commonElements.settingTab.click();
         waitForVisibilityOf(settingElements.logInOrSignUp);
         loginElements.assertValue(settingElements.logInOrSignUp, Constants.LOGIN.LOG_IN_OR_SIGN_UP);
 
-        //Click Log In or Sign Up
         settingElements.logInOrSignUp.click();
         Utils.waitForElementVisible(appiumDriver, loginElements.actionBarTitle);
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
 
 
-        //Click Forgot Password link
+        //2. Click "Forgot password?" link
         loginElements.forgotPasswordLink.click();
         //Check Reset Password screen displays
         Assert.assertTrue(Utils.checkElementExist(loginElements.forgotPasswordMessage),"Check forgot message displays");
 
-        //Enter valid Email format
+
+        //3. Enter valid email to Email field
         loginElements.setValue(loginElements.email, Constants.LOGIN_EMAIL);
 
-        //Click Send Email Email
+
+        //4. Click "Send Mail" button
         loginElements.sendEmailButton.click();
         Utils.sleep(Constants.SHORTTIME);
-        //Check Success screen display
         Assert.assertTrue(Utils.checkElementExist(loginElements.successImage));
         loginElements.assertValue(loginElements.successMessage, Constants.LOGIN.SEND_EMAIL_SUCCESS_MESSAGE);
 
-        //Click Back icon
+
+        //5. Click "Back" icon button on left top app bar.
         loginElements.closeActionButton.click();
-        //Check Login screen
         loginElements.assertValue(loginElements.actionBarTitle, Constants.LOGIN.LOG_IN_TITLE);
     }
 }
