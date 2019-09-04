e615989056476cf46cfd15829bab124a5c117910
Sarah <spham@inquirer.com>
2019-09-04 17:15:16 +0700

    Correct iOS hash app id

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final String BS_USERNAME = "perriduncan1";
     public static final String BS_ACCESSKEY = "FXQUqgscz3gX5NzxH61k";
 
-    public static final String IOS_HASHED_APP_ID = "e176ded8b3b1d845dfcc8fb8dc4177ca11c1911a";
+    public static final String IOS_HASHED_APP_ID = "1891e353e58624c9387d4c3edb4955427aaf1a56";
     public static final String ANDROID_HASHED_APP_ID = "61a1e476416ff76e227118278a40b56a42e78466";
 
     public static final int TIME_OUT = 5;
