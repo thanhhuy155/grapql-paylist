5bc7e1f792907cb03a7e5794f24ec355fd865954
Sarah <spham@inquirer.com>
2019-10-30 08:51:57 +0700

    AndroidTestCases: Disable running  all tests

--- src/test/java/tests/AndroidTestCases.java
+++ src/test/java/tests/AndroidTestCases.java
@@ -1,197 +1,195 @@
 package tests;
 
-import org.testng.annotations.Test;
-
 public class AndroidTestCases extends BaseTestClass{
     // ============== General ============== //
-    @Test
-    public void S228_C20166_GN_LA_001_VerifyAppBeAbleToLaunchOnCellConnection() {
-        generalTest.S228_C20166_GN_LA_001_VerifyAppBeAbleToLaunchOnCellConnection();
-    }
-
-    @Test
-    public void S228_C20167_GN_LA_002_VerifyAppBeAbleToLaunchWithoutConnectivity() {
-        generalTest.S228_C20167_GN_LA_002_VerifyAppBeAbleToLaunchWithoutConnectivity();
-    }
-
-    @Test
-    public void S228_C20168_GN_LA_003_VerifyAppBeAbleToLaunchWithoutConnectivityAndNoCache() {
-        generalTest.S228_C20168_GN_LA_003_VerifyAppBeAbleToLaunchWithoutConnectivityAndNoCache();
-    }
-
-    @Test
-    public void S228_C20169_GN_LA_004_VerifyAppBeAbleToLaunchOnWifi() {
-        generalTest.S228_C20169_GN_LA_004_VerifyAppBeAbleToLaunchOnWifi();
-    }
-
-    @Test
-    public void S228_C20170_GN_LA_005_VerifyAppBeAbleToLOpenViaComingOutOfSleepMode() {
-        generalTest.S228_C20170_GN_LA_005_VerifyAppBeAbleToLOpenViaComingOutOfSleepMode();
-    }
-
-    @Test
-    public void S228_C20171_GN_ART_001_VerifyAppWorksFineWhenClickingOnArticle() {
-        generalTest._VerifyAppWorksFineWhenClickingOnArticle();
-    }
-
-
-     //============== HOME TAB ============== //
-    @Test
-    public void S203_C18616_HT_TC_001_VerifyTopCarouselArticle() {
-        homeTab.S203_C18616_HT_TC_001_VerifyTopCarouselArticle();
-    }
-
-    @Test
-    public void S203_C20164_HT_HF_001_VerifyHomeFeedArticleClick() {
-        homeTab.S203_C20164_HT_HF_001_VerifyHomeFeedArticleClick();
-    }
-
-    @Test
-    public void HomeTabTCNo8() {
-        homeTab.TestCaseNo8();
-    }
-
-     //============== SECTION TAB ============== //
-     @Test
-     public void SectionTabTCNo2() {
-         sectionTab.TestCaseNo2();
-     }
-
-    @Test
-    public void SectionTabTCNo3() {
-        sectionTab.TestCaseNo3();
-    }
-
-    @Test
-    public void SectionTabTCNo4() {
-        sectionTab.TestCaseNo4();
-    }
-
-    @Test
-    public void SectionTabTCNo6() {
-        sectionTab.TestCaseNo6();
-    }
-
-    @Test
-    public void SectionTabTCNo8() {
-        sectionTab.TestCaseNo8();
-    }
-
-    @Test
-    public void SectionTabTCNo11() {
-        sectionTab.TestCaseNo11();
-    }
-
-    @Test
-    public void SectionTabTCNo12() {
-        sectionTab.TestCaseNo12();
-    }
-
-     //============== BOOKMARK TAB ============== //
-    @Test
-    public void BookMarkTCNo2() {
-        bookmarkTab.TestCaseNo2();
-    }
-
-    @Test
-    public void BookMarkTCNo3() {
-        bookmarkTab.TestCaseNo3();
-    }
-
-    @Test
-    public void BookMarkTCNo11() {
-        bookmarkTab.TestCaseNo11();
-    }
-
-    @Test
-    public void BookMarkTCNo12(){
-        bookmarkTab.TestCaseNo12();
-    }
-
-    @Test
-    public void BookMarkTCNo14() {
-        bookmarkTab.TestCaseNo14();
-    }
-
-    @Test
-    public void BookMarkTCNo16(){
-        bookmarkTab.TestCaseNo16();
-    }
-
-     //============== ARTICLE PAGE ============== //
-    @Test
-    public void ArticleTCNo1() {
-        articlePage.TestCaseNo1();
-    }
-
-    @Test
-    public void ArticleTCNo5() {
-        articlePage.TestCaseNo5();
-    }
-
-    @Test
-    public void ArticleTCNo4(){
-        articlePage.TestCaseNo4();
-    }
-
-    @Test
-    public void ArticleTCNo30(){
-        articlePage.TestCaseNo30();
-    }
-
-    //============== SETTING TAB ============== //
-    @Test
-    public  void S229_C20176_ST_001_VerifyNotificationOnOffWorksWell(){
-        settingTab.S229_C20176_ST_001_VerifyNotificationsOnOffWorksWell();
-    }
-
-    @Test
-    public void SettingTCNo3(){settingTab.TestCaseNo3();}
-
-    @Test
-    public  void S229_C20177_ST_002_VerifyMenuAboutDisplayWhenClickingAbout(){
-        settingTab.S229_C20177_ST_002_VerifyMenuAboutDisplayWhenClickingAbout();
-    }
-
-    @Test
-    public  void S229_C20178_ST_003_VerifyAppsAppearsWhenClickMoreApss(){
-        settingTab.S229_C20178_ST_003_VerifyAppsAppearsWhenClickMoreApss();
-    }
-
-    @Test
-    public void S229_C20179_ST_004_VerifyInquirerAppVersion(){
-        settingTab.S229_C20179_ST_004_VerifyInquirerAppVersion();
-    }
-
-    @Test
-    public void S229_C20180_ST_005_VerifyInquirerCopyright(){
-        settingTab.S229_C20180_ST_005_VerifyInquirerCopyright();
-    }
-
-    //============== RATING MODULE ============== //
-    @Test
-    public void RatingModuleTCNo1(){
-        ratingModule.TestCaseNo1();
-    }
-
-    @Test
-    public void RatingModuleTCNo2(){
-        ratingModule.TestCaseNo2();
-    }
-
-    @Test
-    public void RatingModuleTCNo3(){
-        ratingModule.TestCaseNo3();
-    }
-
-    @Test
-    public void RatingModuleTCNo4(){
-        ratingModule.TestCaseNo4();
-    }
-
-    @Test
-    public void RatingModuleTCNo5(){
-        ratingModule.TestCaseNo5();
-    }
+//    @Test
+//    public void S228_C20166_GN_LA_001_VerifyAppBeAbleToLaunchOnCellConnection() {
+//        generalTest.S228_C20166_GN_LA_001_VerifyAppBeAbleToLaunchOnCellConnection();
+//    }
+//
+//    @Test
+//    public void S228_C20167_GN_LA_002_VerifyAppBeAbleToLaunchWithoutConnectivity() {
+//        generalTest.S228_C20167_GN_LA_002_VerifyAppBeAbleToLaunchWithoutConnectivity();
+//    }
+//
+//    @Test
+//    public void S228_C20168_GN_LA_003_VerifyAppBeAbleToLaunchWithoutConnectivityAndNoCache() {
+//        generalTest.S228_C20168_GN_LA_003_VerifyAppBeAbleToLaunchWithoutConnectivityAndNoCache();
+//    }
+//
+//    @Test
+//    public void S228_C20169_GN_LA_004_VerifyAppBeAbleToLaunchOnWifi() {
+//        generalTest.S228_C20169_GN_LA_004_VerifyAppBeAbleToLaunchOnWifi();
+//    }
+//
+//    @Test
+//    public void S228_C20170_GN_LA_005_VerifyAppBeAbleToLOpenViaComingOutOfSleepMode() {
+//        generalTest.S228_C20170_GN_LA_005_VerifyAppBeAbleToLOpenViaComingOutOfSleepMode();
+//    }
+//
+//    @Test
+//    public void S228_C20171_GN_ART_001_VerifyAppWorksFineWhenClickingOnArticle() {
+//        generalTest._VerifyAppWorksFineWhenClickingOnArticle();
+//    }
+//
+//
+//     //============== HOME TAB ============== //
+//    @Test
+//    public void S203_C18616_HT_TC_001_VerifyTopCarouselArticle() {
+//        homeTab.S203_C18616_HT_TC_001_VerifyTopCarouselArticle();
+//    }
+//
+//    @Test
+//    public void S203_C20164_HT_HF_001_VerifyHomeFeedArticleClick() {
+//        homeTab.S203_C20164_HT_HF_001_VerifyHomeFeedArticleClick();
+//    }
+//
+//    @Test
+//    public void HomeTabTCNo8() {
+//        homeTab.TestCaseNo8();
+//    }
+//
+//     //============== SECTION TAB ============== //
+//     @Test
+//     public void SectionTabTCNo2() {
+//         sectionTab.TestCaseNo2();
+//     }
+//
+//    @Test
+//    public void SectionTabTCNo3() {
+//        sectionTab.TestCaseNo3();
+//    }
+//
+//    @Test
+//    public void SectionTabTCNo4() {
+//        sectionTab.TestCaseNo4();
+//    }
+//
+//    @Test
+//    public void SectionTabTCNo6() {
+//        sectionTab.TestCaseNo6();
+//    }
+//
+//    @Test
+//    public void SectionTabTCNo8() {
+//        sectionTab.TestCaseNo8();
+//    }
+//
+//    @Test
+//    public void SectionTabTCNo11() {
+//        sectionTab.TestCaseNo11();
+//    }
+//
+//    @Test
+//    public void SectionTabTCNo12() {
+//        sectionTab.TestCaseNo12();
+//    }
+//
+//     //============== BOOKMARK TAB ============== //
+//    @Test
+//    public void BookMarkTCNo2() {
+//        bookmarkTab.TestCaseNo2();
+//    }
+//
+//    @Test
+//    public void BookMarkTCNo3() {
+//        bookmarkTab.TestCaseNo3();
+//    }
+//
+//    @Test
+//    public void BookMarkTCNo11() {
+//        bookmarkTab.TestCaseNo11();
+//    }
+//
+//    @Test
+//    public void BookMarkTCNo12(){
+//        bookmarkTab.TestCaseNo12();
+//    }
+//
+//    @Test
+//    public void BookMarkTCNo14() {
+//        bookmarkTab.TestCaseNo14();
+//    }
+//
+//    @Test
+//    public void BookMarkTCNo16(){
+//        bookmarkTab.TestCaseNo16();
+//    }
+//
+//     //============== ARTICLE PAGE ============== //
+//    @Test
+//    public void ArticleTCNo1() {
+//        articlePage.TestCaseNo1();
+//    }
+//
+//    @Test
+//    public void ArticleTCNo5() {
+//        articlePage.TestCaseNo5();
+//    }
+//
+//    @Test
+//    public void ArticleTCNo4(){
+//        articlePage.TestCaseNo4();
+//    }
+//
+//    @Test
+//    public void ArticleTCNo30(){
+//        articlePage.TestCaseNo30();
+//    }
+//
+//    //============== SETTING TAB ============== //
+//    @Test
+//    public  void S229_C20176_ST_001_VerifyNotificationOnOffWorksWell(){
+//        settingTab.S229_C20176_ST_001_VerifyNotificationsOnOffWorksWell();
+//    }
+//
+//    @Test
+//    public void SettingTCNo3(){settingTab.TestCaseNo3();}
+//
+//    @Test
+//    public  void S229_C20177_ST_002_VerifyMenuAboutDisplayWhenClickingAbout(){
+//        settingTab.S229_C20177_ST_002_VerifyMenuAboutDisplayWhenClickingAbout();
+//    }
+//
+//    @Test
+//    public  void S229_C20178_ST_003_VerifyAppsAppearsWhenClickMoreApss(){
+//        settingTab.S229_C20178_ST_003_VerifyAppsAppearsWhenClickMoreApss();
+//    }
+//
+//    @Test
+//    public void S229_C20179_ST_004_VerifyInquirerAppVersion(){
+//        settingTab.S229_C20179_ST_004_VerifyInquirerAppVersion();
+//    }
+//
+//    @Test
+//    public void S229_C20180_ST_005_VerifyInquirerCopyright(){
+//        settingTab.S229_C20180_ST_005_VerifyInquirerCopyright();
+//    }
+//
+//    //============== RATING MODULE ============== //
+//    @Test
+//    public void RatingModuleTCNo1(){
+//        ratingModule.TestCaseNo1();
+//    }
+//
+//    @Test
+//    public void RatingModuleTCNo2(){
+//        ratingModule.TestCaseNo2();
+//    }
+//
+//    @Test
+//    public void RatingModuleTCNo3(){
+//        ratingModule.TestCaseNo3();
+//    }
+//
+//    @Test
+//    public void RatingModuleTCNo4(){
+//        ratingModule.TestCaseNo4();
+//    }
+//
+//    @Test
+//    public void RatingModuleTCNo5(){
+//        ratingModule.TestCaseNo5();
+//    }
 
 }
